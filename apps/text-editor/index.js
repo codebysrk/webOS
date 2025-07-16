0abcb197b725026079476b996f863224487d3f0b
codebysrk <maishahrukhh@gmail.com>
2025-07-16 19:25:17 +0530

    deleted calculater and text-editor apps

--- apps/text-editor/index.js
+++
@@ -1,8 +0,0 @@
-const editor = document.getElementById("editorArea");
-const saveBtn = document.getElementById("saveBtn");
-const saveMsg = document.getElementById("saveMsg");
-
-saveBtn.addEventListener("click", () => {
-  saveMsg.textContent = "Saved! (Not persistent)";
-  setTimeout(() => (saveMsg.textContent = ""), 1500);
-});
